a5ded1cbe71115424c563e0af6b36c3bec37e2b9
Mert Can Altın <37827216+mertcanaltin@users.noreply.github.com>
2018-05-22 17:13:37 +0300

    Add files via upload

---
+++ randevuform.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim istenilen() As String = {"Mr", "Ultrason", "tomografi"}
+        ComboBox1.Items.AddRange(istenilen)
+        ComboBox1.Items.Insert(0, "Seçiniz")
+        ComboBox1.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        Dim secilentarih As Date = DateTimePicker1.Value
+
+        ListBox1.Items.Add(ComboBox1.SelectedItem & ": " & secilentarih)
+
+    End Sub
+End Class
